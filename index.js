a356111fa61798baa2b651f42ec5d9896df745d8
Gabriel Abreu <120991872+gabrielvsabreu@users.noreply.github.com>
2025-08-10 17:17:23 -0300

    commit teste

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Rota de teste
 app.get("/", (req, res) => {
   res.send("API está viva!");
 });
 
-// Rota de exemplo
-app.get("/ping", (req, res) => {
-  res.json({ status: "ok" });
-});
-
-// Inicializa o servidor
 app.listen(port, "0.0.0.0", () => {
   console.log(`✅ API rodando na porta ${port}`);
 });
